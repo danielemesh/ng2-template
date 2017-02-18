5166f96e53e17293a9d4c01823a2a72f2edb19be
daniel <danielemesh@gmail.com>
2017-02-18 18:13:35 +0200

    Moved all services images into one folder

--- src/app/balloons/shared/mock-balloons.ts
+++ src/app/balloons/shared/mock-balloons.ts
@@ -4,25 +4,25 @@ export const Balloons: Balloon[] = [
   {
     title: "Tickets",
     description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text",
-    imageUrl: "assets/images/tickets.png",
+    imageUrl: "assets/images/services/tickets.png",
     bgColor: "#4eb9f4"
   },
   {
     title: "Camera",
     description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text",
-    imageUrl: "assets/images/camera.png",
+    imageUrl: "assets/images/services/camera.png",
     bgColor: "#6fc125"
   },
   {
     title: "Beer",
     description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text",
-    imageUrl: "assets/images/beermug.png",
+    imageUrl: "assets/images/services/beermug.png",
     bgColor: "#ff5649"
   },
   {
     title: "Devices",
     description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text",
-    imageUrl: "assets/images/ipad.png",
+    imageUrl: "assets/images/services/ipad.png",
     bgColor: "#e48f23"
   }
 ];
